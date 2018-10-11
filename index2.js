d30881d43cd022c8a5ba6b0936deef06ecf0a08e
Jose Almada <jalmada.e@gmail.com>
2018-10-11 00:47:08 -0400

    Example branch

--- index2.js
+++ index2.js
@@ -6,7 +6,8 @@ var newPosition = movementEngine.GetNextPosition(
     {x:10, y:10},
     {x:21, y:21},
     {x:1, y:1},
-    {x:10, y:10}
+    {x:10, y:10},
+    {x:9999, y:9999}
 );
 
 console.log(`X: ${newPosition.x}, Y:${newPosition.y}`);
